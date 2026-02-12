4545f9bebb260b1003943fcdffb13bb5656b701b
Freeeemon <simon.dousset23@gmail.com>
2026-02-12 16:07:49 +0100

    ci: fix 2.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,32 @@
-// eslint.config.js
 import js from "@eslint/js";
+import globals from "globals";
+import cypress from "eslint-plugin-cypress/flat";
 
 export default [
   js.configs.recommended,
   {
     files: ["assets/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       "no-console": "warn",
       "no-unused-vars": "warn",
     },
+  },
+  {
+    files: ["commitlint.config.js", "cypress.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["tests/e2e/**/*.js", "cypress/**/*.js"],
+    ...cypress.configs.recommended,
   },
   {
     ignores: ["node_modules/", "vendor/", "public/", "var/"],
