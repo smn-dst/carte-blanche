f54e890cde8933c472d51022717e1e0d41c3a815
Simon <99979047+smn-dst@users.noreply.github.com>
2026-02-13 11:00:35 +0100

    Optimize CI GitHub Actions (#49)
    * test
    
    * ci: test githooks & ci
    
    * ci: fix ci & githooks
    
    * ci: fix 2.0
    
    * ci: fix cs-check
    
    * ci: fix cd-fixer
    
    * ci: fix build & artefacts setup
    
    * ci: fix lint php/js for download artefacts
    
    * ci: fix vendor
    
    * ci: fix artifact
    
    * ci: add assets/vendor in eslint config
    
    * ci: fix port e2e run
    
    * ci: fix time execution CI
    
    * ci: fix artifact

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import cypress from "eslint-plugin-cypress/flat";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["assets/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    files: ["commitlint.config.js", "cypress.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["tests/e2e/**/*.js", "cypress/**/*.js"],
+    ...cypress.configs.recommended,
+  },
+  {
+    ignores: ["node_modules/", "vendor/", "public/", "var/", "assets/vendor/"],
+  },
+];
