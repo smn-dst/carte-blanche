d051583405b7102980828ab3d6d80f60bf70c42c
Vidocq17 <AvidanB1704@gmail.com>
2026-02-17 13:54:36 +0100

    feat: integrate Tailwind CSS and add sidebar component

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
