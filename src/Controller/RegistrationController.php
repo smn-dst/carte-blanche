63d85bf7209438f6a938d624d1e415bb54579936
Vidocq17 <AvidanB1704@gmail.com>
2026-02-20 17:24:58 +0100

    feat: enhance registration and login forms with improved styling and functionality

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -39,7 +39,7 @@ class RegistrationController extends AbstractController
         }
 
         return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
