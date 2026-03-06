a677148097b6a6e1c04808c6fb3feb9b49745e4b
Vidocq17 <AvidanB1704@gmail.com>
2026-03-06 20:29:21 +0100

    feat: fix for cs-fixer 4

--- src/Controller/EncheresController.php
+++ src/Controller/EncheresController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\FavoriteRepository;
 use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
