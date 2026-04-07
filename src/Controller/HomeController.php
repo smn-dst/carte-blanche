4fca1f54d58b45b75987fca063eb42f24bceb827
bastosjt <151185735+bastosjt@users.noreply.github.com>
2026-04-07 19:15:11 +0200

    feat: added favorites page front & back (#94)
    * feat: added favorites page front & back
    
    * feat: added favorites toggle button

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FavoriteRepository;
 use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(RestaurantRepository $restaurantRepository): Response
+    public function index(RestaurantRepository $restaurantRepository, FavoriteRepository $favoriteRepository): Response
     {
         $featuredRestaurants = $restaurantRepository->findFeaturedForHome(5);
 
+        $favoriteRestaurantIds = [];
+        $user = $this->getUser();
+        if ($user instanceof \App\Entity\User) {
+            $favoriteRestaurantIds = $favoriteRepository->findRestaurantIdsByUser($user);
+        }
+
         return $this->render('home/index.html.twig', [
             'featuredRestaurants' => $featuredRestaurants,
+            'favoriteRestaurantIds' => $favoriteRestaurantIds,
         ]);
     }
 }
