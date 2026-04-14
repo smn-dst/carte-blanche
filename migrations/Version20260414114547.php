165a71dfac89c61e62c9190331d582bc5c067f8e
Vidocq17 <AvidanB1704@gmail.com>
2026-04-14 20:22:32 +0200

    refactor: remove notification fields from user migration

--- migrations/Version20260414114547.php
+++ migrations/Version20260414114547.php
@@ -16,17 +16,9 @@ final class Version20260414114547 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE "user" ADD notif_new_auctions BOOLEAN DEFAULT NULL');
-        $this->addSql('ALTER TABLE "user" ADD notif_reminders BOOLEAN DEFAULT NULL');
-        $this->addSql('ALTER TABLE "user" ADD notif_results BOOLEAN DEFAULT NULL');
-        $this->addSql('ALTER TABLE "user" ADD notif_newsletter BOOLEAN DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE "user" DROP notif_new_auctions');
-        $this->addSql('ALTER TABLE "user" DROP notif_reminders');
-        $this->addSql('ALTER TABLE "user" DROP notif_results');
-        $this->addSql('ALTER TABLE "user" DROP notif_newsletter');
     }
 }
